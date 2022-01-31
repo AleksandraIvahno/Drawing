0c8dd79fa7822f85e8f0b36e01e3a8f14047525e
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-01-31 12:55:59 +0000

    #4

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 import turtle
-snowflake = turtle.Turtle()
-for i in range(10):
-  for i in range(2):
-    snowflake.forward(100)
-    snowflake.right(60)
-    snowflake.forward(100)
-    snowflake.right(120)
-  snowflake.right(36)
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+
+tess = turtle.Turtle()
+tess.color("hotpink")
+tess.pensize(5)
+
+
+alex = turtle.Turtle()
+
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+
+tess.right(180)
+tess.forward(80)
+
+
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+for x in range(4):
+
+  alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+for i in range(4):
+  tess.left(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.forward(80)
+tess.left(120)
+tess.right(111)
