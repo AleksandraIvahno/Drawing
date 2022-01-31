56e452dd3d2ef2174fdfb11b1fa049f80dfbaf97
AleksandraIvahn <91884342+AleksandraIvahno@users.noreply.github.com>
2022-01-31 12:29:05 +0000

    #3

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
 import turtle
-
-def draw_square(some_turtle):
-
-    for i in range (1,5):
-        some_turtle.forward(200)
-        some_turtle.right(90)
-
-def draw_art():
-    window = turtle.Screen()
-    window.bgcolor("black")
-    #Turtle Brad
-    brad = turtle.Turtle()
-    brad.shape("turtle")
-    brad.color("yellow")
-    brad.speed(6)
-    brad.pensize(2)
-    for i in range(1,37):
-        draw_square(brad)
-        brad.right(10)
-    #Turtle Angie
-    angie = turtle.Screen()
-    angie.shape("turtle")
-    angie.color("blue")
-    angie.speed(5)
-    angie.pensize(2)
-    size=1
-    while (True):
-        angie.forward(size)
-        angie.right(91)
-        size = size + 1
-
-    window.exitonclick()
-
-draw_art()
+snowflake = turtle.Turtle()
+for i in range(10):
+  for i in range(2):
+    snowflake.forward(100)
+    snowflake.right(60)
+    snowflake.forward(100)
+    snowflake.right(120)
+  snowflake.right(36)
